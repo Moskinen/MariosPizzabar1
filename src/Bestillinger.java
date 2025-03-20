d97fd6731af837be81abbaf5a5e5ccaa0c4f68fd
Yakoob1884 <vivja.aty@gmail.com>
2025-03-20 09:42:45 +0100

    Formattering

--- src/Bestillinger.java
+++ src/Bestillinger.java
@@ -1,53 +1,78 @@
 //Mo kode starter her grundet github fejl
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 public class Bestillinger {
 
-        private String name;
-        private int number;
-        private double price;
-        private int amount;
-        private List <Bestillinger> bestillingsListe;
+    private String name;
+    private int number;
+    private double price;
+    private int amount;
+    private List<Bestillinger> bestillingsListe;
 
-        public Bestillinger(String name, int number, double price, int amount){
-            this.name = name;
-            this.number = number;
-            this.price = price;
-            this.amount = amount;
-        }
-        public String getName(){ return name; }
-        public int getNumber(){ return number; }
-        public double getPrice(){ return price; }
-        public int getAmount(){ return amount; }
+    public Bestillinger(String name, int number, double price, int amount) {
+        this.name = name;
+        this.number = number;
+        this.price = price;
+        this.amount = amount;
+    }
 
-        public void setName(String name){ this.name = name; }
-        public void setNumber(int number){ this.number = number; }
-        public void setPrice(double price){ this.price = price; }
-        public void setAmount(int amount){ this.amount = amount; }
+    public String getName() {
+        return name;
+    }
 
-        @Override
-        public String toString(){
-            return "Bestilling: " + name + " | Nummer: " + number + " | Pris: " + price + " | Antal: " + amount;
-        }
+    public int getNumber() {
+        return number;
+    }
 
-        public void bestillingsListe(){
-            this.bestillingsListe = new ArrayList <>();
-        }
+    public double getPrice() {
+        return price;
+    }
 
-        public void tilføjBestilling(Bestillinger bestilling){
-            bestillingsListe.add(bestilling);
-        }
-        public void visBestillinger(){
-            if (bestillingsListe.isEmpty()) {
-                System.out.println("Ingen aktive bestillinger");
-            }else{
-                for (Bestillinger b : bestillingsListe){
-                    System.out.println(b);
-                }
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    @Override
+    public String toString() {
+        return "Bestilling: " + name + " | Nummer: " + number + " | Pris: " + price + " | Antal: " + amount;
+    }
+
+    public void bestillingsListe() {
+        this.bestillingsListe = new ArrayList<>();
+    }
+
+    public void tilføjBestilling(Bestillinger bestilling) {
+        bestillingsListe.add(bestilling);
+    }
+
+    public void visBestillinger() {
+        if (bestillingsListe.isEmpty()) {
+            System.out.println("Ingen aktive bestillinger");
+        } else {
+            for (Bestillinger b : bestillingsListe) {
+                System.out.println(b);
             }
         }
     }
-    //Mo kode afslutter her
+}
+//Mo kode afslutter her
 
