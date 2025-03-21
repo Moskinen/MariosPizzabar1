b501cd09930c2df7a389c083f35e3bbc0a353e1d
Moskinen <mortenlyngsie@gmail.com>
2025-03-21 15:19:21 +0100

    k

--- src/Main.java
+++ src/Main.java
@@ -91,7 +91,9 @@ public class Main {
         Bestillinger bestilling = new Bestillinger (customerName, orderNumber, pickupTime);
         boolean addingPizzaer = true;
         while (addingPizzaer) {
-            loadMenuItems();
+            System.out.println("===== PIZZA MENU =====");
+            for (Pizza pizza : menuItems) {
+                System.out.println(pizza);
             int pizza1 = 0;
             System.out.println("Hvilken pizza vil du tilføje: 0-14");
             int choice = scanner.nextInt();
